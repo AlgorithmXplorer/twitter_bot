c64d42b466e50e452ea18435bd112f47378ee645
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-05-04 01:12:42 +0300

    init function is wrote

--- bot.py
+++ bot.py
@@ -6,3 +6,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 
+
+class bot:
+    
+    def __init__(self,username:str , email:str , password:str):
+        self.option = Options()
+        self.option.add_experimental_option("prefs",{"intl.accept_languages" : "en"})
+        self.driver = webdriver.Chrome(options= self.option)
+        self.action = ActionChains(driver=self.driver)
+        
+        self.email = email
+        self.username = username
+        self.password = password
+
+    
+
+
